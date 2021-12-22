a5f88207d7f8e6397d418d788bb39e8f24231b94
DYernar <eduisebai.1@gmail.com>
2021-12-22 16:58:16 +0600

    removed docker compose

--- db/scripts/000003_create_users_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id bigserial PRIMARY KEY,
-    iin text UNIQUE NOT NULL,
-    login text UNIQUE NOT NULL,
-    password text NOT NULL,
-    created_at timestamp(0) WITH TIME ZONE NOT NULL DEFAULT NOW()
-);
